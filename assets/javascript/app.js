cc17568325ba74f0598a1702f3f99ef798e3b56b
Kaushik Nath <kaush.kn.735@gmail.com>
2021-07-18 22:15:21 +0530

    improved javascript form validations

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,14 +18,121 @@ function toggleMenu () {
    }
 }
 
+// Adding a Loader
+
+let spinnerWrapper = document.querySelector('.spinner-wrapper');
+window.addEventListener('load', function () {
+    // spinnerWrapper.style.display = 'none';
+    spinnerWrapper.parentElement.removeChild(spinnerWrapper);
+});
+
 // Form Validation
 
-const form = document.querySelector('.form')
-const fullName = document.querySelector('.fullName')
-const mobileNo = document.querySelector('.mobileNo')
-const email = document.querySelector('.email')
-const password = document.querySelector('.password')
-const confirmPassword = document.querySelector('.confirmPassword')
+const form = document.querySelector("form")
+const fullName = document.querySelector('.fullName');
+const mobileNo = document.querySelector('.mobileNo');
+const email = document.querySelector('.email');
+const password = document.querySelector('.password');
+const cPassword = document.querySelector('.confirm-password');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    formValidate();
+})
+
+const sendData = (nameVal,sRate,count) => {
+    if(sRate === count) {
+        alert('Registration Success');
+        swal("Welcome " + nameVal, "Registration Success", "success");
+        // location.href = `demo.html?userrname=${nameVal}`;
+    }
+}
+
+const successMsgs = (nameVal) => {
+     let formCon = document.getElementsByClassName('form-control');
+     var count = formCon.length - 1;
+     for(var i = 0; i < formCon.length; i++) {
+         if(formCon[i].className === 'form-control success') {
+            var sRate = 0 + i; 
+            sendData(nameVal,sRate,count);
+         }else{
+             return false;
+         }
+     }
+}
+const isEmail = (emailVal) => {
+    var atSymbol = emailVal.indexOf('@');
+    if(atSymbol < 1) return false;
+    var dot = emailVal.lastIndexOf('.');
+    if(dot <= atSymbol + 3) return false;
+    if(dot === emailVal.length - 1) return false;
+    return true;
+}
+
+function formValidate(){
+    const nameVal = fullName.value.trim();
+    const mobVal = mobileNo.value.trim();
+    const emailVal = email.value.trim();
+    const passwordVal = password.value.trim();
+    const cPasswordVal = cPassword.value.trim();
+    
+
+    if(nameVal === ''){
+        setErrMsg(fullName,'enter a name value');
+    }else if(nameVal.length <= 2){
+        setErrMsg(fullName, 'length should be more than 2 characters');
+    }else{
+        setSuccessMsg(fullName);
+    }
+
+    if(mobVal === ''){
+        setErrMsg(mobileNo,'enter a mobile value');
+    }else if(mobVal.length != 10){
+        setErrMsg(mobileNo, 'length should not be less than 10');
+    }else{
+        setSuccessMsg(mobileNo);
+    }
+
+    if(emailVal === ''){
+        setErrMsg(email,'enter a email value');
+    }else if(!isEmail(emailVal)){
+        setErrMsg(email, 'email is not valid');
+    }else{
+        setSuccessMsg(email);
+    }
+
+    if(passwordVal === ''){
+        setErrMsg(password,'enter a password value');
+    }else if(passwordVal.length < 5){
+        setErrMsg(password, 'password should be more than 5 characters');
+    }else{
+        setSuccessMsg(password);
+    }
+
+    if(cPasswordVal === ''){
+        setErrMsg(cPassword,'enter a password value');
+    }else if(passwordVal != cPasswordVal){
+        setErrMsg(cPassword, 'password do not match');
+    }else{
+        setSuccessMsg(cPassword);
+    }
+    successMsgs(nameVal);
+}
+
+
+function setErrMsg(input, errMsg) {
+    const formControl = input.parentElement;  
+    const small = formControl.querySelector('small'); 
+    small.innerText = errMsg;
+    formControl.className = 'form-control error';
+}
+
+function setSuccessMsg(input){
+    formControl = input.parentElement; 
+    formControl.className = 'form-control success';
+    const small = formControl.querySelector('small'); 
+    small.innerText = " ";
+}
 
 
 
